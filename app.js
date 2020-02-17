59c422393ccacfa443f8f9545038227571c74d57
Kellen <Kellen@Keiths-MBP.attlocal.net>
2020-02-16 17:13:12 -0800

    created randomPhraseArray function.

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-/* 
-In this project, you'll create a word guessing game. Players will click letters from an onscreen keyboard to try to guess a random phrase. The player’s goal is to guess all the letters in the phrase. A player can keep choosing letters until they make five incorrect guesses. Letters guessed correctly will appear in the phrase. Letters guessed incorrectly will be counted at the bottom of the screen.
-*/
-
-// GLOBALS
+// Globals
 const qwerty = document.querySelector('qwerty');
 const phrase = document.querySelector('phrase');
 
@@ -13,14 +9,24 @@ const startOverlay = document.querySelector('#overlay');
 const phrases = 
     [ 
     'If you spend too much time thinking about a thing, you\'ll never get it done.', 
-    'Do not pray for an easy life, pray for the strength to endure a difficult one', 
+    'Do not pray for an easy life, pray for the strength to endure a difficult one.', 
     'I fear not the man who has practices 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.', 
     'Real living is living for others.', 
     'The successful warrior is the average man, with laser-like focus.'  
     ]
 
-// Start Game
+const newCharacters = [];
+
 startGame.addEventListener('click', () => {
     // Hide the start screen overlay 
     startOverlay.style.display = 'none';
 });
+
+
+function getRandomPhraseAsArray(arr) {
+    // do stuff to any arr that is passed in
+    const randomItem = phrases[Math.floor(Math.random() * phrases.length)];
+    console.log(randomItem);
+}
+getRandomPhraseAsArray(phrases);
+
