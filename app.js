a5d97a9f1309fa9bb3b2089a05bfa088e6c2dde9
Kellen <Kellen@Keiths-MBP.attlocal.net>
2020-02-17 23:41:44 -0800

    added conditional IF statement to addPhraseToDisplay function.

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const missed = 0;
 const startGame = document.querySelector('.btn__reset');
 const startOverlay = document.querySelector('#overlay');
 const phrases = 
-    [ 
+[ 
     'If you spend too much time thinking about a thing, you\'ll never get it done.', 
     'Do not pray for an easy life, pray for the strength to endure a difficult one.', 
     'I fear not the man who has practices 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.', 
     'Real living is living for others.', 
     'The successful warrior is the average man, with laser-like focus.'  
-    ]
+]
 
 startGame.addEventListener('click', () => {
     // Hide the start screen overlay 
@@ -36,12 +36,18 @@ const newCharacters = getRandomPhraseAsArray(phrases).split("");
 function addPhraseToDisplay(arr) {
     // do stuff any arr that is passed in, and add to `#phrase ul`
     for (let i = 0; i < newCharacters.length; i++) {
-        console.log(newCharacters[i]);
-        let text = newCharacters[i];
-        const ul = document.getElementById('phrase');
-        const li = document.createElement('li');
-        li.textContent = text;
-        ul.appendChild(li); 
+        if (newCharacters.length > 0) {
+            console.log(newCharacters[i]);
+            let text = newCharacters[i];
+            const ul = document.getElementById('phrase');
+            const li = document.createElement('li');
+            li.textContent = text;
+            ul.appendChild(li);
+            if (newCharacters[i].trim()) {
+                // is a letter
+                li.classList.add('letter');
+            } 
+        }
     }
 }
 addPhraseToDisplay();
