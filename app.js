d71dea6f4f1fe9e6dba5fcb1761d7bf8c208b7f5
Kellen <Kellen@Keiths-MBP.attlocal.net>
2020-02-17 23:06:28 -0800

    addPhraseToDisplay function is populating data to screen.

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const phrase = document.querySelector('.phrase');
 const missed = 0;
 const startGame = document.querySelector('.btn__reset');
 const startOverlay = document.querySelector('#overlay');
-
-let newCharacters;
-
 const phrases = 
     [ 
     'If you spend too much time thinking about a thing, you\'ll never get it done.', 
@@ -17,7 +14,7 @@ const phrases =
     'The successful warrior is the average man, with laser-like focus.'  
     ]
 
-    startGame.addEventListener('click', () => {
+startGame.addEventListener('click', () => {
     // Hide the start screen overlay 
     startOverlay.style.display = 'none';
 });
@@ -27,14 +24,26 @@ function getRandomPhraseAsArray(arr) {
     return phrases[Math.floor(Math.random() * phrases.length)];
 }
 // Split that phrase into a new array of characters
-newCharacters = getRandomPhraseAsArray(phrases).split();
-console.log(newCharacters);
+const newCharacters = getRandomPhraseAsArray(phrases).split("");
 
-
+/* 
+1. Create an addPhraseToDisplay function that loops through an array of characters.
+2. Inside the loop, for each character in the array, you’ll create a list item.
+3. Put the character inside of the list item.
+4. Append that list item to the #phrase ul in your HTML.
+*/ 
 
 function addPhraseToDisplay(arr) {
     // do stuff any arr that is passed in, and add to `#phrase ul`
-    
-
+    for (let i = 0; i < newCharacters.length; i++) {
+        console.log(newCharacters[i]);
+        let text = newCharacters[i];
+        const ul = document.getElementById('phrase');
+        const li = document.createElement('li');
+        li.textContent = text;
+        ul.appendChild(li); 
+    }
 }
+addPhraseToDisplay();
+
 
