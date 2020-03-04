488eaf79f089423033a5557e8c17f5a9b58e09b2
Kellen James <Kellenkjames@gmail.com>
2020-03-03 16:39:43 -0800

    Line 26 - Updated getRandomPhraseArrray to be a LET variable.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ let missed = 0;
 let headline = document.querySelector('.title');
 
 // Return a random phrase from the array
-const getRandomPhraseAsArray = arr => {
+let getRandomPhraseAsArray = arr => {
     // Randomly choose a phrase from the phrases array 
     return phrases[Math.floor(Math.random() * phrases.length)].toLowerCase().split("");
 }
